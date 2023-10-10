91c7c2f646319d3f2a22642824fc524d5f953d03
bidawang <mistarlong@gmail.com>
2023-10-10 14:45:44 +0700

    Danang belajar github

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+})
+export class Tab4Page implements OnInit {
+
+  constructor() {
+    
+  }
+  showPersonalInfo1 = false;
+  showPersonalInfo2 = false;
+  showPersonalInfo3 = false;
+  showPersonalInfo4 = false;
+  ngOnInit(): void {
+  }
+  
+
+}
